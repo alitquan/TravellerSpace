0e7d55bcd2f8ad816a0873cd19b518c8616f039e
alitquan <alitmallick@gmail.com>
2022-05-29 14:35:08 -0400

    Resized images, loads initial images, automatic slideshow

--- flaskr/static/js/functions.js
+++ flaskr/static/js/functions.js
@@ -25,4 +25,16 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-} 
+}
+
+function autoSlide() {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(autoSlide, 3500); // Change image every 2 seconds
+}
