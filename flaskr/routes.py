a5fe012c47c97de8bb997a9fc5d919233e909509
alitquan <alitmallick@gmail.com>
2022-07-06 13:05:03 -0400

    Registration form work

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -7,12 +7,19 @@ from flaskr.db import get_db
 
 bp = Blueprint ('routes',__name__)
 
-
-
 @bp.route('/')
 def index():
-    return render_template('main/first.html') 
+    print("hey")
+    return render_template('main/first.html')
 
-@bp.route('/register')
+@bp.route('/register', methods=['POST','GET'])
 def reg():
+    if request.method == 'GET':
+        _username=request.form.get('username')
+        _password=request.form.get('password')
+        _confPassword=request.form.get('confPassword')
+        _name = request.form.get('name')
+        _email = request.form.get('email')
+        _country = request.form.get('country')
+        print(_username, _password,_confPassword,_name,_email,_country)
     return render_template('auth/registration.html')
