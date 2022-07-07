162f5059ba34e5b7e9af31d83f4c221b60d5031e
alitquan <alitmallick@gmail.com>
2022-07-06 23:20:49 -0400

    Registration validation -- do passwords match and are they valid? Added pipfile. Concern --- flashes happen twice

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -1,10 +1,9 @@
 from flask import (
         Blueprint, flash, g, redirect, render_template, request, url_for)
 from werkzeug.exceptions import abort
-
 from flaskr.auth import login_required
 from flaskr.db import get_db
-
+import re
 bp = Blueprint ('routes',__name__)
 
 @bp.route('/')
@@ -14,12 +13,34 @@ def index():
 
 @bp.route('/register', methods=['POST','GET'])
 def reg():
-    if request.method == 'GET':
+    if request.method == 'POST':
         _username=request.form.get('username')
         _password=request.form.get('password')
         _confPassword=request.form.get('confPassword')
         _name = request.form.get('name')
         _email = request.form.get('email')
         _country = request.form.get('country')
-        print(_username, _password,_confPassword,_name,_email,_country)
+        if (_password!=_confPassword):
+            flash("Passwords do not match",category="error")
+        if (not validatePassword(_password)):
+            flash("Password needs at least one number and at least one special character",category="error")
     return render_template('auth/registration.html')
+
+
+#auxilary methods 
+def validatePassword(value):
+    numbers=False
+    special=False
+
+    special_characters = "!@#$%^&*()-+?_=,<>/"
+    for character in value:
+        if character.isdigit():
+            numbers=True
+            print ("Numbers detected")
+        if (character in special_characters):
+            special=True
+            print("yes")
+    if (special == True and numbers == True):
+        return True
+    else:
+        return False
