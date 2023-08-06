ade1f3e9ea1a3e639e13a3a1c7674074f1c13b43
alitquan <alitmallick@gmail.com>
2023-08-05 22:28:49 -0400

    Added Profiles and Reviews table. Profile record auto-created upon registration

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,9 @@
-DROP TABLE IF EXISTS Persons; 
+SET FOREIGN_KEY_CHECKS=0;
 DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Profiles;
+DROP TABLE IF EXISTS Reviews;
+DROP TABLE IF EXISTS Persons;
+SET FOREIGN_KEY_CHECKS=1;
 CREATE TABLE Persons (
     PersonID int,
     LastName varchar(255),
@@ -14,6 +18,23 @@ CREATE TABLE Users (
     nickname varchar(20) NOT NULL,
     email varchar(20) NOT NULL,
     country varchar(30) NOT NULL,
+    PRIMARY KEY(id), 
     UNIQUE(id),
     UNIQUE(username)
+);
+CREATE TABLE Profiles (
+    user_id int NOT NULL, 
+    bio MEDIUMTEXT DEFAULT '',
+    locations MEDIUMTEXT DEFAULT '',
+    ratings int DEFAULT 0 NOT NULL,
+    PRIMARY KEY (user_id),
+    FOREIGN KEY (user_id) REFERENCES Users(id)
+);
+CREATE TABLE Reviews( 
+    review_id int AUTO_INCREMENT, 
+    reviewer_id int NOT NULL,
+    reviewed_id int NOT NULL, 
+    body MEDIUMTEXT NOT NULL, 
+    rating int NOT NULL,
+    PRIMARY KEY (review_id)
 );
